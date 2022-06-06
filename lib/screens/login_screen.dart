0ad0f4c060118fb261d9d6233d85d7ae076b7be5
Marco Antônio Patrocinio <73245980+MarcoPatrocinio@users.noreply.github.com>
2022-06-06 16:59:49 -0300

    Alteração Logo e SignIn

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,11 +4,53 @@ class LoginScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Row(
-      children: [
-        Text('Text'),
-        Text('Text 2')
-      ],
+    return Scaffold(
+      body:
+      Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+
+        child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Center(
+                child: Flex(
+                    direction: Axis.vertical,
+                    children: [
+                      Container(
+                        height: 150,
+                        margin: const EdgeInsets.all(25),
+                        child: const Image(image: AssetImage('assets/images/Logo.png')),
+                      ),
+                      Container(
+                        height: 200,
+                        margin: const EdgeInsets.all(25),
+                        child: Form(
+                          child: ListView(
+                            children: [
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    labelText: 'Email'
+                                ),
+                                textInputAction: TextInputAction.next,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    labelText: 'Senha'
+                                ),
+                                textInputAction: TextInputAction.done,
+                                obscureText: true,
+                              ),
+                              ElevatedButton(onPressed: (() => ''), child: Text('Prosseguir'))
+                            ],
+                          ),
+                        ),
+                      )
+                    ]
+                )
+            )
+        ),
+      )
     );
   }
 }
